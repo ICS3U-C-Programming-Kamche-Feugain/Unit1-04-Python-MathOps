8f8da7545b248aa4f38f2f0837122eb300fa2a27
Kamche Feugain Sofo <kamche.feugain-sofo@stu.ocsb.ca>
2026-09-16 22:43:46 +0000

    First commit to github of basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2026 Kamche All rights reserved.
+// .
+// Created by: Your Kamche
+// Date: Sep 15, 2026
+// This program is used to calculate basic math.
+
+#include <iostream>
+
+int main() {
+    std::cout << "7 + 4 =" << 7 + 4 << std::endl;  // addition;
+    std::cout << "7 - 4 =" << 7 - 4 << std::endl;    // subtraction;
+    std::cout << "7 * 4 =" << 7 * 4 << std::endl;  // multiplication;
+    std::cout << "8 / 4 =" << 8 / 4 << std::endl;  // division;
+    std::cout << "7.0 / 2 =" << (float)7 / 2 << std::endl;  // decimal;
+}
