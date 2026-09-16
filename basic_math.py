e64bcf68e120a3445b3b8bd45e38fafbaeb0a6ad
Kamche Feugain Sofo <kamche.feugain-sofo@stu.ocsb.ca>
2026-09-16 13:31:23 +0000

    Added to github repository

---
+++ basic_math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Created By:Kamche
+# Date: September 16, 2026
+# This program performs basic math operations.
+
+
+def main():
+    print(f"2 + 3 = {2 + 3}")
+    print(f"5 - 4 = {5 - 4}")
+    print(f"7 * 6 = {7 * 6}")
+    print(f"8 / 3 = {8 / 3}")
+    print(f"3 ** 2 = {3 ** 2}")
+    print(f"4 ** 0.5 = {4 ** 0.5}")
+
+
+if __name__ == "__main__":
+    main()
