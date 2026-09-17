c12ccb2cf901639778a8c10b087ad1a307a51692
Kamche Feugain Sofo <kamche.feugain-sofo@stu.ocsb.ca>
2026-09-17 13:43:44 +0000

    First commit basicMath.cpp to github

--- basicMath.cpp
+++ basicMath.cpp
@@ -5,11 +5,15 @@
 // This program is used to calculate basic math.
 
 #include <iostream>
-
+#include <cmath>
 int main() {
-    std::cout << "7 + 4 =" << 7 + 4 << std::endl;  // addition;
-    std::cout << "7 - 4 =" << 7 - 4 << std::endl;    // subtraction;
-    std::cout << "7 * 4 =" << 7 * 4 << std::endl;  // multiplication;
-    std::cout << "8 / 4 =" << 8 / 4 << std::endl;  // division;
-    std::cout << "7.0 / 2 =" << (float)7 / 2 << std::endl;  // decimal;
+    std::cout << "2 + 3 =" << 2 + 3 << std::endl;  // addition;
+    std::cout << "8 - 4 =" << 8 - 4 << std::endl;    // subtraction;
+    std::cout << "8 * 2 =" << 8 * 2 << std::endl;  // multiplication;
+    std::cout << "10/2 =" << 10/2 << std::endl;  //  integer division;
+    std::cout << "7/2 =" << 7.0/2 << std::endl;  // decimal division;
+    std::cout << "11 % 7 =" << 11 % 7 << std::endl;           // modulo;
+    std::cout << "5 ^ 2 =" << pow(5, 2) << std::endl;  // exponents;
+    std::cout << "16 ^ 0.5 =" << pow(16, 0.5) << std::endl;  // exponents;
+    std::cout << " √16 =" << sqrt(16) << std::endl;  // square root;
 }
