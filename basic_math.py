c0eb2e6bb9b74b9d32f55b959a577ed52a125d37
Kamche Feugain Sofo <kamche.feugain-sofo@stu.ocsb.ca>
2026-09-17 13:30:07 +0000

    First commit basic_math.py to github

--- basic_math.py
+++ basic_math.py
@@ -3,14 +3,15 @@
 # Date: September 16, 2026
 # This program performs basic math operations.
 
-
 def main():
-    print(f"2 + 3 = {2 + 3}")
-    print(f"5 - 4 = {5 - 4}")
-    print(f"7 * 6 = {7 * 6}")
-    print(f"8 / 3 = {8 / 3}")
-    print(f"3 ** 2 = {3 ** 2}")
-    print(f"4 ** 0.5 = {4 ** 0.5}")
+    print("2 + 3 = {}". format(2 + 3)) # addition
+    print("6 - 3 = {}". format(6 - 3)) # subtraction
+    print("8 * 2 = {}". format(8 * 2)) # multiplication
+    print("7 / 2 = {}". format(7 / 2)) # division
+    print("3 ** 2 = {}". format(3 ** 2)) # exponentiation
+    print("16 ** 0.5 = {}". format(16 ** 0.5)) # square root
+    print("1+3*4 = {}". format(1 + 3 * 4)) # bedmas 
+    print("10 % 7 = {}". format(10 % 7)) # modulo (remainder)
 
 
 if __name__ == "__main__":
